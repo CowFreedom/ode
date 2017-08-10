7cc9b87560b12c784dc29c95f7d61fae803588c7
CowFreedom <tristan.scheidemann@live.com>
2017-08-10 19:27:47 +0200

    Add files via upload
    New version with variable step size control.

---
+++ Source.cpp
@@ -0,0 +1,216 @@
+#include <iostream>
+#include <array>
+#include "rode.h"
+#include <functional> // für std::function
+#include <vector>
+#include <ctime> //um performance zu messen
+#include <cmath>//um Funktionen zu testen in testalgorithms()
+
+template<class T, class M>
+void printTrajectory(std::pair<M,T> trajectory) {
+	std::cout << "t:\tu:\n";
+	for (size_t i = 0; i < (trajectory.second).size(); i++) {
+		std::cout << (trajectory.first)[i] << "\t";
+		for (size_t j = 0; j < (trajectory.second)[i].size(); j++) {
+			std::cout << (trajectory.second)[i][j] << "\t";
+		}
+		std::cout << "\n";
+	}
+}
+
+template<class T, class M>
+void saveTrajectory(std::pair<M, T> trajectory) {
+
+	FILE *f;
+
+	errno_t errorCode = fopen_s(&f, "C:/Users/Tristan_local/Documents/Visual Studio 2015/Projects/ODE/rode/trajectories/test.txt", "w");
+
+	if (f == NULL) {
+
+		printf("Error opening file!\n");
+		std::cin.get();
+		exit(1);
+	}
+
+	for (size_t i = 0; i < (trajectory.second).size(); i++) {
+		fprintf(f, "%lf\t", (trajectory.first)[i]);
+		for (size_t j = 0; j < (trajectory.second)[i].size(); j++) {
+			fprintf(f, "%lf\t", trajectory.second[i][j]);
+		}
+		fprintf(f, "\n");
+	}
+
+	printf("Everything worked!\n");
+	fclose(f);
+}
+
+
+double f1(std::vector<double> u, double t)  {
+	return 0.02*u[0]*u[1];
+}
+
+
+double f2(std::vector<double> u, double t)  {
+	return u[1];
+}
+
+void testlotkavolterra() {
+	double gamma = 1.5;
+	double s = 0.01;
+	double c = 0.8;
+	double delta = 2;
+	double r = s*c;
+	double sq = 0.02;
+	double cq = 0.4;
+	double inhibition = 0.001;
+
+	auto u1 = [&](std::vector<double>&& u, double t) 
+	{return gamma*u[0] * (1 - (u[0] * inhibition)) - s*u[0] * u[1]; };
+
+	auto u2 = [&](std::vector<double>&& u, double t)
+	{return -delta*u[1] + r*u[0] * u[1]; };
+
+	std::vector<std::function<double(std::vector<double>, double)>> functions;
+	functions.push_back(u1);
+	functions.push_back(u2);
+	//std::array<[]double(*)(std::vector<double>, double), 2> a = { u1,u2 };
+	std::array<double, 2> tspan = { 0,60 };
+	std::array<double, 2> u0 = { 200,100 };
+
+	
+	clock_t begin = std::clock();
+	r::ode4(functions, tspan, u0, 0.001);
+
+	clock_t end = clock();
+	double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+	std::cout << "Zeit ODE1:" << elapsed_secs << "\n";
+
+	begin = std::clock();
+	r::ode1_old(functions, tspan, u0, 0.001);
+	end = clock();
+	elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+	std::cout << "Zeit ODE2:" << elapsed_secs << "\n";
+
+	std::array<double, 2> h_values = { 0.00001,5 };
+//	auto result = r::ode45(functions, tspan, u0, h_values);
+	std::cout << "DONE!\n";
+//	printTrajectory(r::ode2(functions, tspan, u0, 0.001));
+//	printTrajectory(r::ode4(functions, tspan, u0, 0.001));
+	//printTrajectory(r::ode1(functions, { 0,30 }, { 200,100 }, 0.001));
+	//printTrajectory(r::ode1(functions, tspan, u0, 0.001));
+
+//	printTrajectory(r::ode1(functions, { 0,30 }, { 200,100 }, 0.001));
+//	saveTrajectory(r::ode2(functions, tspan, u0, 0.001));
+	//printTrajectory(r::ode1({ f1,f2 }, { 1,5 }, { -1,2 }, 1));
+}
+
+void testalgorithms() {
+	double h = 0.001;
+	double t_start = 0;
+	double t = t_start;
+
+	std::array<double, 2> tspan = { t_start,10 };
+	std::array<double, 2> u0 = { 1,1 };
+	auto u1 = [&](std::vector<double>&& u, double t) {
+		return u[1];
+	};
+	auto u2 = [&](std::vector<double>&& u, double t) {
+		return -u[0];
+	};
+	std::vector<std::function<double(std::vector<double>, double)>> functions;
+	functions.push_back(u1);
+	functions.push_back(u2);
+
+	auto r1 = [&](double t) {
+		return exp(t);
+	};
+	auto r2 = [&](double t) {
+		return exp(-t);
+	};
+	std::vector<std::function<double(double)>> real_functions;
+	real_functions.push_back(r1);
+	real_functions.push_back(r2);
+	std::cout << "Globaler Fehler (u-y)/h:\n";
+	//auto result = r::ode2(functions, tspan, u0, h);
+	std::array<double, 2> h_values = { 0.01,5 };
+	auto result = r::ode45(functions, tspan, u0, h_values);
+	std::cout << "t:\tu:\n";
+	for (size_t i = 0; i < (result.second).size(); i++) {
+		auto t = (result.first)[i];
+		std::cout << (result.first)[i] << "\t";
+		for (size_t j = 0; j < (result.second)[i].size(); j++) {
+			std::cout << (result.second)[i][j] - (real_functions[j](t)) << "\t";
+		}
+		std::cout << "\n";
+	}
+//	printTrajectory(result);
+}
+
+void test_adaptive() {
+	double h_min = 1.0E-6;
+	double h_max = 5;
+	double t_start = -5;
+	double t = t_start;
+
+	std::array<double, 2> tspan = { -5,5 };
+	std::array<double, 2> u0 = { 3.9968E-4 };
+	auto u1 = [&](std::vector<double>&& u, double t) {
+		return -200 * t*pow(u[0],2);
+	};
+	std::vector<std::function<double(std::vector<double>, double)>> functions;
+	functions.push_back(u1);
+
+	std::array<double, 2> h_values = { h_min,h_max };
+	auto result = r::ode45(functions, tspan, u0, h_values);
+//	printTrajectory(result);
+
+	//Tatschliche Funktion
+	auto r1 = [&](double t) {
+		return (0.01)/(pow(t,2)+0.020016);
+	};
+	std::vector<std::function<double(double)>> real_functions;
+	real_functions.push_back(r1);
+	std::cout << "Globaler Fehler (u-y)/h:\n";
+	std::cout << "t:\tu:\n";
+	for (size_t i = 0; i < (result.second).size(); i++) {
+		auto t = (result.first)[i];
+		std::cout << t << "\t";
+		std::cout << (result.second)[i][0] - (real_functions[0](t)) << "\t";
+		std::cout << "\n";
+	}
+
+}
+
+void sample_ode() {
+	double h = 0.2;
+	double t_start = 0;
+	double t = t_start;
+	double t_end = 8;
+
+	std::array<double, 2> tspan = { t_start,t_end };
+	std::array<double, 2> u0 = { 1,1 };
+
+	auto u1 = [&](std::vector<double>&& u, double t) {
+		return t;
+	};
+	auto u2 = [&](std::vector<double>&& u, double t) {
+		return -u[1];
+	};
+	std::vector<std::function<double(std::vector<double>, double)>> functions;
+	functions.push_back(u1);
+	functions.push_back(u2);
+	std::array<double, 2> h_values = { 0.02,1 };
+	auto result = r::ode45(functions, tspan, u0, h_values);
+
+		printTrajectory(result);
+}
+
+int main() {
+
+	testlotkavolterra();
+//	test_adaptive();
+//	testalgorithms();
+//	sample_ode();
+
+	std::cin.get();
+}
---
+++ rode.h
@@ -0,0 +1,111 @@
+#pragma once
+
+#ifndef rode
+
+namespace r {
+
+	/*Interfaces*/
+	/*Runge Kutta Interface*/
+	template<class Derived>
+	class enable_down_cast {
+	private:
+		typedef enable_down_cast Base;
+	public:
+		Derived const* self() const {
+			return static_cast<Derived const*>(this);
+		}
+		Derived* self() {
+			return static_cast<Derived*>(this);
+		}
+	protected:
+		~enable_down_cast() = default;
+	};
+
+	template<class Impl, class rk_Container1D, class rk_Container2D>
+	class RKTableInterface :public enable_down_cast<Impl> {
+
+	private:
+		using enable_down_cast<Impl>::self;
+
+	public:
+		rk_Container2D getA() { return self()->do_getA(); }
+		rk_Container1D getC() { return self()->do_getC(); }
+		rk_Container1D getB_P() { return self()->do_getB_P(); }
+		rk_Container1D getB_Plus() { return self()->do_getB_Plus(); }
+		unsigned int getOrder() { return self()->do_getOrder(); }
+
+		void setB_Plus(rk_Container1D&) { self()->do_setB_Plus(); };
+
+		void setB_P(rk_Container1D&) { self()->do_setB_P(); };
+
+		template<class T>
+		void setC(const std::initializer_list<T>&&) { self()->do_setC(const std::initializer_list<T>&&); };
+
+		template<class T>
+		void setA(const std::initializer_list<std::initializer_list<T>>) { self()->do_setA(const std::initializer_list<std::initializer_list<T>>); };
+
+		template<class T>
+		void setB_P(const std::initializer_list<T>&&) { self()->do_setB_P(const std::initializer_list<T>&&); };
+
+		template<class T>
+		void setB_Plus(const std::initializer_list<T>&&) { self()->do_setB_Plus(const std::initializer_list<T>&&); };
+
+
+	protected:
+		~RKTableInterface() = default;
+		rk_Container2D a;
+		rk_Container1D b_p;
+		rk_Container1D b_plus;
+		rk_Container1D c;
+
+	};
+
+	/*Template für explizite Runge Kutta Verfahren ohne Schrittweitensteuerung.*/
+	template<class fContainer, class numberContainer, class initContainer, class T, class alg, class rk_Container1D, class rk_Container2D >
+	auto Explicit_RKTemplate(RKTableInterface<alg,rk_Container1D, rk_Container2D>& butcher_tableau, const fContainer& u, const numberContainer& tspan, const initContainer& u0, T h);
+
+	template<class fContainer, class numberContainer, class initContainer, class alg, class rk_Container1D, class rk_Container2D, class measure>
+	auto Explicit_variable_RKTemplate(RKTableInterface<alg, rk_Container1D, rk_Container2D>& butcher_tableau, const fContainer& u, const numberContainer& tspan, const initContainer& u0, const numberContainer& h_values, measure rule);
+
+	/*
+	Algorithmus: Euler (explizit)
+	Ordnung des Verfahrens: 1
+	Adaptive Schrittweite: Nein
+
+	Diese Funktion nutzt den expliziten Euler, ein Verfahren erster Ordnung.
+	Aufgrund der vergleichsweise geringen Genauigkeit sollte dieser Algorithmus nur für einfachste ODE's
+	genutzt werden oder wenn Performance wichtig ist.*/
+	template<class fContainer, class numberContainer, class initContainer, class T>
+	auto ode1_old(const fContainer& u, const numberContainer& tspan,const initContainer& u0, T h);
+
+//	template<class fContainer, class numberContainer, class initContainer, class T>
+//	auto ode1(const std::initializer_list<fContainer>& u, const std::initializer_list<numberContainer>& tspan, const std::initializer_list<initContainer>& u0, T h);
+
+	template<class fContainer, class numberContainer, class initContainer, class T, class M>
+	M ode1_old(fContainer& u, const std::initializer_list<numberContainer>& tspan, const std::initializer_list<initContainer>& u0, T h);
+
+	template<class fContainer, class numberContainer, class initContainer, class T>
+	auto ode1(const fContainer& u, const numberContainer& tspan, const initContainer& u0, T h);
+
+	//Heun
+	template<class fContainer, class numberContainer, class initContainer, class T>
+	auto ode2(const fContainer& u, const numberContainer& tspan, const initContainer& u0, T h);
+
+
+	template<class fContainer, class numberContainer, class initContainer, class T>
+	auto ode4(const fContainer& u, const numberContainer& tspan, const initContainer& u0, T h);
+
+	template<class fContainer, class numberContainer, class initContainer>
+	auto ode45(const fContainer& u, const numberContainer& tspan, const initContainer& u0, const numberContainer& h_values);
+}
+
+
+
+
+#include "rk_tables.tpp"
+
+#include "rode.tpp"
+
+#include "r_auxilliary.tpp"
+
+#endif
